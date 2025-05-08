0c31903b61ad16ee5c7e43bfef4732d4bf1b1e18
Tejas-b463 <108131310+Tejas-b463@users.noreply.github.com>
2025-05-08 15:04:29 +0530

    update the code frontend client/

--- server/src/index.js
+++ server/src/index.js
@@ -24,11 +24,11 @@ connectDB();
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")))
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-    })
+        res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+    });
 }
 
 // Start the server
